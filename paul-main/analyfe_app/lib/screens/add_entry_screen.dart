d9a8d36475b1dce9162cd256bf368740f8dc9efc
Paul Schmidt <paulbenschmidt@gmail.com>
2020-06-27 08:46:14 -0500

    UI Theme Tweaks and Adding Time Scroll

--- paul-main/analyfe_app/lib/screens/add_entry_screen.dart
+++ paul-main/analyfe_app/lib/screens/add_entry_screen.dart
@@ -1,64 +1,73 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 
 import '../style/soft_ui.dart';
 
-class AddEntryScreen extends StatelessWidget {
+class AddEntryScreen extends StatefulWidget {
+  @override
+  _AddEntryScreenState createState() => _AddEntryScreenState();
+}
+
+class _AddEntryScreenState extends State<AddEntryScreen> {
+  DateTime _dateTime = DateTime.now();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).backgroundColor,
       appBar: PreferredSize(
-        preferredSize: Size.fromHeight(100.0),
-        child: AppBar(
-          backgroundColor: Theme.of(context).backgroundColor,
-          elevation: 0,
-          centerTitle: true,
-          bottom: PreferredSize(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                Container(
-                  child: FlatButton(
-                    onPressed: () {},
-                    child: Icon(Icons.chevron_left),
-                  ),
-                  decoration: SoftUi.boxDecoration(context),
-                ),
-                Text(
-                  'Entry',
-                  style: Theme.of(context).textTheme.headline1,
-                ),
-                Container(
-                  child: FlatButton(
-                    onPressed: () {},
-                    child: Icon(Icons.save_alt),
-                  ),
-                  decoration: SoftUi.boxDecoration(context),
-                ),
-              ],
-            ),
-            preferredSize: null,
-          ),
-        ),
+        preferredSize: Size.fromHeight(75.0),
+        child: entryAppBar(context),
       ),
       body: ListView(
         children: [
-          SizedBox(
-            height: 50,
-          ),
+          SizedBox(height: 20),
+          Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+            Container(
+                width: 350,
+                height: 135,
+                decoration: SoftUi.boxDecoration(context),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Container(
+                      child: Center(
+                        child: Text(
+                          'Wake',
+                          style: Theme.of(context).textTheme.bodyText1,
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 85,
+                      width: 150,
+                      child: CupertinoDatePicker(
+                        mode: CupertinoDatePickerMode.time,
+                        onDateTimeChanged: (dateTime) {
+                          setState(() {
+                            dateTime = _dateTime;
+                          });
+                        },
+                      ),
+                    ),
+                  ],
+                )),
+          ]),
+          SizedBox(height: 20),
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Container(
-                width: 300,
-                height: 200,
+                width: 350,
+                height: 155,
+                decoration: SoftUi.boxDecoration(context),
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: [
                     Padding(
                       padding: EdgeInsets.all(20),
                       child: Text(
-                        'Sleep',
+                        'Learning',
                         style: Theme.of(context).textTheme.bodyText1,
                       ),
                     ),
@@ -85,6 +94,27 @@ class AddEntryScreen extends StatelessWidget {
                     ),
                   ],
                 ),
+              ),
+            ],
+          ),
+          SizedBox(height: 20),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                width: 350,
+                height: 155,
+                decoration: SoftUi.boxDecoration(context),
+              ),
+            ],
+          ),
+          SizedBox(height: 20),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                width: 350,
+                height: 155,
                 decoration: SoftUi.boxDecoration(context),
               ),
             ],
@@ -93,4 +123,40 @@ class AddEntryScreen extends StatelessWidget {
       ),
     );
   }
+}
+
+AppBar entryAppBar(context) {
+  return AppBar(
+    backgroundColor: Theme.of(context).backgroundColor,
+    elevation:0,
+    centerTitle: true,
+    bottom: PreferredSize(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Container(
+            child: FlatButton(
+              onPressed: () {},
+              child: Icon(Icons.chevron_left),
+            ),
+            decoration: SoftUi.boxDecoration(context),
+            width: 60,
+          ),
+          Text(
+            'Entry',
+            style: Theme.of(context).textTheme.headline1,
+          ),
+          Container(
+            child: FlatButton(
+              onPressed: () {},
+              child: Icon(Icons.file_upload),
+            ),
+            decoration: SoftUi.boxDecoration(context),
+            width: 60,
+          ),
+        ],
+      ),
+      preferredSize: null,
+    ),
+  );
 }
--- paul-main/analyfe_app/lib/style/theme_data.dart
+++ paul-main/analyfe_app/lib/style/theme_data.dart
@@ -1,98 +1,26 @@
 import 'package:flutter/material.dart';
 
-final ThemeData defaultTheme = ThemeData(
-  primarySwatch: Colors.pink,
-  accentColor: Colors.amber,
-  backgroundColor: Colors.grey[200],
-  highlightColor: Colors.grey[400],
-  dividerColor: Colors.white,
-  textTheme: ThemeData.light().textTheme.copyWith(
-        headline1: TextStyle(
-          color: Colors.blueGrey[700],
-          fontFamily: 'Montserrat',
-          fontWeight: FontWeight.w600,
-          fontSize: 25,
+final ThemeData defaultTheme = _buildDefaultTheme();
+
+ThemeData _buildDefaultTheme() {
+  return ThemeData(
+    primarySwatch: Colors.pink,
+    accentColor: Colors.amber,
+    backgroundColor: Colors.grey[300],
+    highlightColor: Colors.grey[500],
+    dividerColor: Colors.white,
+    textTheme: ThemeData.light().textTheme.copyWith(
+          headline1: TextStyle(
+            color: Colors.blueGrey[700],
+            fontFamily: 'Montserrat',
+            fontWeight: FontWeight.w600,
+            fontSize: 18,
+          ),
+          bodyText1: TextStyle(
+            fontFamily: 'Montserrat',
+            color: Colors.blueGrey[700],
+            fontSize: 18,
+          ),
         ),
-        bodyText1: TextStyle(
-          fontFamily: 'Montserrat',
-          color: Colors.blueGrey[700],
-          fontSize: 25,
-        ),
-      ),
-);
-
-// TextTheme _buildDefaultTextTheme(TextTheme base) {
-//   return base.copyWith(
-//     headline: base.headline.copyWith(
-//       fontWeight: FontWeight.w500,
-//     ),
-//     title: base.title.copyWith(
-//       color: Colors.black,
-//       fontWeight: FontWeight.w700,
-//       fontSize: 30.0,
-//     ),
-//     caption: base.caption.copyWith(
-//       fontWeight: FontWeight.w400,
-//       fontSize: 14.0,
-//     ),
-//     body2: base.body2.copyWith(
-//       fontWeight: FontWeight.w500,
-//       fontSize: 16.0,
-//     ),
-//   );
-// }
-
-// ThemeData(
-//         primarySwatch: Colors.pink,
-//         accentColor: Colors.amber,
-//         backgroundColor: Colors.grey[200],
-//         highlightColor: Colors.grey[400],
-//         dividerColor: Colors.white,
-//         textTheme: ThemeData.light().textTheme.copyWith(
-//               headline1: TextStyle(
-//                 color: Colors.blueGrey[700],
-//                 fontFamily: 'Montserrat',
-//                 fontWeight: FontWeight.w600,
-//                 fontSize: 25,
-//               ),
-//               bodyText1: TextStyle(
-//                 fontFamily: 'Montserrat',
-//                 color: Colors.blueGrey[700],
-//                 fontSize: 25,
-//               ),
-//             ),
-//       ),
-
-// import 'package:flutter/material.dart';
-
-// final ThemeData defaultTheme = _buildDefaultTheme();
-
-// ThemeData _buildDefaultTheme() {
-//   final ThemeData base = ThemeData.light();
-//   return base.copyWith(
-//     textTheme: _buildDefaultTextTheme(base.textTheme),
-//     primaryTextTheme: _buildDefaultTextTheme(base.primaryTextTheme),
-//     accentTextTheme: _buildDefaultTextTheme(base.accentTextTheme),
-//   );
-// }
-
-// TextTheme _buildDefaultTextTheme(TextTheme base) {
-//   return base.copyWith(
-//     headline: base.headline.copyWith(
-//       fontWeight: FontWeight.w500,
-//     ),
-//     title: base.title.copyWith(
-//       color: Colors.black,
-//       fontWeight: FontWeight.w700,
-//       fontSize: 30.0,
-//     ),
-//     caption: base.caption.copyWith(
-//       fontWeight: FontWeight.w400,
-//       fontSize: 14.0,
-//     ),
-//     body2: base.body2.copyWith(
-//       fontWeight: FontWeight.w500,
-//       fontSize: 16.0,
-//     ),
-//   );
-// }
+  );
+}
